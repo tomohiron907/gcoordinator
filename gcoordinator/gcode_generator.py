ddaffc7fde4ef15c1bc9427dc4c6dd46cb46e169
Taniguchi Tomohiro <tomohiron907@gmail.com>
2023-10-13 23:11:05 +0900

    add initial settings method and changed save method in gcode_generator

--- gcoordinator/gcode_generator.py
+++ gcoordinator/gcode_generator.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-from gcoordinator.print_settings import *
+from gcoordinator import print_settings 
 
 
 class GCode:
@@ -34,50 +34,77 @@ class GCode:
         """
         txt = ''
         for path in self.full_object:
-            txt += f'G0 F{TRAVEL_SPEED} X{path.x[0]+X_ORIGIN} Y{path.y[0]+Y_ORIGIN} Z{path.z[0]}\n'
+            # travel to the first point of the path
+            txt += f'G0 F{path.travel_speed} '
+            txt += f'X{path.x[0]+path.x_origin} '
+            txt += f'Y{path.y[0]+path.y_origin} '
+            txt += f'Z{path.z[0]}\n'
             for i in range(len(path.x)-1):
-                txt += f'G1 F{PRINT_SPEED} '
-                txt += f'X{path.x[i+1]+X_ORIGIN} '
-                txt += f'Y{path.y[i+1]+Y_ORIGIN} '
+                # print the path. move to the next point with extrusion
+                txt += f'G1 F{path.print_speed} '
+                txt += f'X{path.x[i+1]+path.x_origin} '
+                txt += f'Y{path.y[i+1]+path.y_origin} '
                 txt += f'Z{path.z[i+1]} '
                 txt += f'E{path.extrusion[i]}\n'
+        return txt
+    
+    def set_initial_settings(self):
+        """
+        Generates G-code commands to set the initial printer settings, such as bed and nozzle temperature, extrusion mode,
+        and fan speed, based on the values defined in the `print_settings` module.
+
+        Returns:
+            str: A string containing the G-code commands to set the initial printer settings.
+        """
+        txt = ''
+        txt += f'M140 S{print_settings.BED_TEMPERATURE} \n'
+        txt += f'M190 S{print_settings.BED_TEMPERATURE} \n'
+        txt += f'M104 S{print_settings.NOZZLE_TEMPERATURE} \n'
+        txt += f'M109 S{print_settings.NOZZLE_TEMPERATURE} \n'
+        txt += f'M83 ;relative extrusion mode \n'
+        txt += f'M106 S{print_settings.FAN_SPEED} \n'
         return txt
 
     def save(self, file_path):
         """
-        Save the generated G-code to a file.
+        Saves the generated G-code to a file.
 
-        Args:
-            file_path (str): The path to the file to save the G-code to.
+        Reads the contents of the start G-code file, generates the initial settings G-code,
+        reads the contents of the end G-code file, combines the start G-code, initial settings G-code,
+        object G-code, and end G-code, and writes the combined G-code to the specified file.
 
-        Returns:
-            None
+        :param file_path: The path of the file to write the G-code to.
+        :type file_path: str
         """
+
+        # Read the contents of the start G-code file
+        with open(self.start_gcode_path, 'r') as f:
+            self.start_gcode_txt = f.read()
+        
+        # Generate the initial settings G-code
+        self.initial_settings_txt = self.set_initial_settings()
+        
+        # Read the contents of the end G-code file
+        with open(self.end_gcode_path, 'r') as f:
+            self.end_gcode_txt = f.read()
+        
+        # Combine the start G-code, initial settings G-code, object G-code, and end G-code
+        self.gcode = (
+            self.start_gcode_txt
+            + self.initial_settings_txt
+            + self.txt
+            + self.end_gcode_txt
+        )
+        
+        # Write the combined G-code to the specified file
         with open(file_path, 'w') as f:
-            f.write(self.txt)
+            f.write(self.gcode)
+        
 
     def start_gcode(self, file_path):
-        """
-        Prepends the contents of a file to the current G-code text.
-
-        Args:
-            file_path (str): The path to the file to be prepended.
-
-        Returns:
-            None
-        """
-        with open(file_path, 'r') as f:
-            self.txt = f.read() + self.txt
+        self.start_gcode_path = file_path
+        
 
     def end_gcode(self, file_path):
-        """
-        Appends the contents of the file at the given file path to the current G-code text.
+        self.end_gcode_path = file_path
 
-        Args:
-            file_path (str): The path to the file to be appended.
-
-        Returns:
-            None
-        """
-        with open(file_path, 'r') as f:
-            self.txt = self.txt + f.read()
--- tests/test.py
+++ tests/test.py
@@ -3,15 +3,12 @@ import numpy as np
 
 
 full_object = []
-for layer in range(500):
+for layer in range(100):
     arg = np.linspace(0, 2*np.pi, 5)
     x = 10 * np.cos(arg)
     y = 10 * np.sin(arg)
     z = np.full_like(x, layer * 0.1)
-    if layer < 50:
-        wall = gc.Path(x, y, z)
-    else:
-        wall = gc.Path(x, y, z)
+    wall = gc.Path(x, y, z)
 
     full_object.append(wall)
 
